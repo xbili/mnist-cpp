dc0d551a1dbbd71f06fbd11bd308485f0825f085
Bili Xu <xubili93@gmail.com>
2017-09-30 20:01:30 +0800

    Implement layer tests

---
+++ test/LayerTests.cpp
@@ -0,0 +1,69 @@
+//
+// Created by Xu Bili on 9/30/17.
+//
+
+#include <gtest/gtest.h>
+#include "../src/Layer.h"
+#include "../src/Neuron.h"
+
+
+class LayerTests : public ::testing::Test {
+
+protected:
+    Layer *layer;
+    virtual void SetUp() {
+        layer = new Layer(4, 4);
+    }
+
+    virtual void TearDown() {
+        delete(layer);
+    }
+};
+
+
+TEST_F(LayerTests, Inititalization) {
+    ASSERT_EQ(layer->getNeuronCount(), 4 + 1); // +1 for the bias neuron
+    ASSERT_EQ(layer->getInputCount(), 4);
+}
+
+
+TEST_F(LayerTests, SetLayerInput) {
+    vector<float> inputs = { 0.1, 0.2, 0.3, 0.4 };
+    for (int i = 0; i < inputs.size(); i++) {
+        layer->setLayerInput(i, inputs[i]);
+    }
+
+    vector<float> layerInputs = layer->getInputs();
+    for (int i = 0; i < layerInputs.size(); i++) {
+        ASSERT_EQ(layerInputs[i], inputs[i]);
+    }
+}
+
+
+TEST_F(LayerTests, SetLayerWeights) {
+    vector<vector<float>> weights = {
+            { 0.1, 0.2, 0.3, 0.4 },
+            { 0.5, 0.6, 0.7, 0.8 },
+            { 0.9, 1.0, 1.1, 1.2 },
+            { 1.3, 1.4, 1.5, 1.6 }
+    };
+
+    vector<float> biasWeights = { 0.1, 0.2, 0.3, 0.4 };
+
+    layer->setWeights(weights);
+    layer->setBiasWeights(biasWeights);
+
+    vector<Neuron*> neurons = layer->getNeurons();
+    for (int i = 0; i < neurons.size() - 1; i++) { // -1 for the bias neuron
+        for (int j = 0; j < 4; j++) {
+            ASSERT_EQ(neurons[i]->getWeight(j), weights[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        ASSERT_EQ(neurons.back()->getWeight(i), biasWeights[i]);
+    }
+}
+
+TEST_F(LayerTests, CalculateLayer) {
+}
