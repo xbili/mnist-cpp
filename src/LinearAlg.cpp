72b460698e0414ada4fa858278f9674dad69010e
Bili Xu <xubili93@gmail.com>
2017-10-24 22:13:24 +0800

    Quantize weights to 8-bit integers

--- src/LinearAlg.cpp
+++ src/LinearAlg.cpp
@@ -3,16 +3,21 @@
 //
 
 #include <assert.h>
+#include <cstdint>
 #include "LinearAlg.h"
 
 float LinearAlg::dotProduct(vector<float> a, vector<float> b) {
     // Sizes of the vectors should be the same
     assert(a.size() == b.size());
 
-    float sum = 0;
+    int sum = 0;
     for (int i = 0; i < a.size(); i++) {
-        sum += a[i] * b[i];
+        int8_t quantizedA = (int8_t) (a[i] * 10);
+        int8_t quantizedB = (int8_t) (b[i] * 10);
+        sum += quantizedA * quantizedB;
     }
 
-    return sum;
+    float back = (float) sum;
+
+    return back / 10;
 }
